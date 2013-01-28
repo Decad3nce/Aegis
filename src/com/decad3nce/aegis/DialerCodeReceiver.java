46b38b9238407ccd4d357fa00ff90f694143f28c
Decad3nce <decad3nce@cyanogenmod.org>
2013-01-27 20:21:40 -0600

    Advanced: Add option to launch aegis from dialer code

---
+++ src/com/decad3nce/aegis/DialerCodeReceiver.java
@@ -0,0 +1,38 @@
+package com.decad3nce.aegis;
+
+import com.decad3nce.aegis.Fragments.AdvancedSettingsFragment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class DialerCodeReceiver extends BroadcastReceiver {
+    private static final String TAG = "aeGis";
+    private static final String SECRET_CODE_ACTION = "android.provider.Telephony.SECRET_CODE";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(SECRET_CODE_ACTION)) {
+            
+            final SharedPreferences preferences = PreferenceManager
+                    .getDefaultSharedPreferences(context);
+            
+            boolean hideFromLauncher  = preferences.getBoolean(
+                    AdvancedSettingsFragment.PREFERENCES_HIDE_FROM_LAUNCHER,
+                    context.getResources().getBoolean(
+                            R.bool.config_default_advanced_hide_from_launcher));
+
+            if(hideFromLauncher) {
+                Intent aegisIntent = new Intent(context, LoginActivity.class);
+                aegisIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        .addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+                        .addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                context.startActivity(aegisIntent);
+            }
+        }
+
+    }
+}
--- src/com/decad3nce/aegis/Fragments/AdvancedSettingsFragment.java
+++ src/com/decad3nce/aegis/Fragments/AdvancedSettingsFragment.java
@@ -1,11 +1,14 @@
 package com.decad3nce.aegis.Fragments;
 
+import com.decad3nce.aegis.AdvancedSettingsActivity;
 import com.decad3nce.aegis.AegisActivity;
 import com.decad3nce.aegis.R;
 
 import android.app.DialogFragment;
 import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
@@ -15,6 +18,7 @@ import android.preference.PreferenceFragment;
 public class AdvancedSettingsFragment extends PreferenceFragment {
     public static final String PREFERENCES_CONFIRMATION_SMS = "advanced_enable_confirmation_sms";
     public static final String PREFERENCES_ABORT_BROADCAST = "advanced_enable_abort_broadcast";
+    public static final String PREFERENCES_HIDE_FROM_LAUNCHER = "advanced_hide_from_launcher";
     private static final String ADVANCED_PREFERENCES_REMOVE_ADMIN = "remove_admin";
     private static final String ADVANCED_PREFERENCES_INSTALL_TO_SYSTEM = "install_to_system";
 
