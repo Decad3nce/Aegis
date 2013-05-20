a96ffcfbcd2f7954447a253e8ecbf39eefded6a8
Decad3nce <decad3nce@cyanogenmod.org>
2013-05-20 01:26:04 -0500

    Android makefile: Update

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
