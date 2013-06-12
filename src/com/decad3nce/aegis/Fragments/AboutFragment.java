d69094e5518e17db9044a37bf94a1310357b73c4
Decad3nce <decad3nce@cyanogenmod.org>
2013-06-12 02:33:18 -0500

    About: Beautify, fix layouts.

--- src/com/decad3nce/aegis/Fragments/AboutFragment.java
+++ src/com/decad3nce/aegis/Fragments/AboutFragment.java
@@ -1,91 +1,154 @@
 package com.decad3nce.aegis.Fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.*;
+import android.widget.*;
 import com.decad3nce.aegis.R;
 
-public class AboutFragment extends Fragment implements OnClickListener {
-    private Button onClickEmail, onClickGithub, onClickShare, onClickXDA, onClickQuickly;
+public class AboutFragment extends Fragment {
+    private Button onClickEmail, onClickGithub, onClickXDA, onClickQuickly;
+    private ShareActionProvider mShareActionProvider;
+    private ListView mButtonList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View mainView = inflater.inflate(R.layout.about_layout, container, false);
+        setHasOptionsMenu(true);
         String version = "UKNOWN";
+        String[] values = new String[] { getResourceString(R.string.about_email_link), getResourceString(R.string.about_github_link), getResourceString(R.string.about_xda_link)};
+        mButtonList = (ListView) mainView.findViewById(R.id.about_list);
+        mButtonList.setAdapter(new aboutImageAdapter(getActivity(), values));
+        mButtonList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            Intent i;
+            String url;
 
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                switch (position) {
+                    case 0:
+                        final Intent emailIntent = new Intent(
+                            android.content.Intent.ACTION_SEND);
+
+                        emailIntent.setType("plain/text");
+                        emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
+                                new String[] { "decad3nce@cyanogenmod.org" });
+                        emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
+                                "aeGis Questions");
+                        startActivity(Intent.createChooser(emailIntent, "Email"));
+                        break;
+                    case 1:
+                        url = "http://www.github.com/Decad3nce/Aegis";
+                        i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        break;
+                    case 2:
+                        url = "http://forum.xda-developers.com/showthread.php?t=2038762";
+                        i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                }
+            }
+        });
         try {
             version = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0).versionName;
         } catch (NameNotFoundException e) {
             //Shouldn't happen
         }
-        TextView tV;
+
+        TextView tV, textView;
         tV = (TextView) mainView.findViewById(R.id.currentversion);
-        onClickEmail = (Button) mainView.findViewById(R.id.emailclick);
-        onClickGithub = (Button) mainView.findViewById(R.id.githubclick);
-        onClickShare = (Button) mainView.findViewById(R.id.shareclick);
-        onClickXDA = (Button) mainView.findViewById(R.id.xdaclick);
-        onClickQuickly = (Button) mainView.findViewById(R.id.quicklyclick);
-        onClickEmail.setOnClickListener(this);
-        onClickGithub.setOnClickListener(this);
-        onClickShare.setOnClickListener(this);
-        onClickXDA.setOnClickListener(this);
-        onClickQuickly.setOnClickListener(this);
+        textView = (TextView) mainView.findViewById(R.id.developername);
         tV.setText(version);
+        tV.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "Roboto-Light.ttf"));
+        textView.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "Roboto-Light.ttf"));
         return mainView;
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent i;
-        String url;
-        switch (v.getId()) {
-            case R.id.emailclick:
-                final Intent emailIntent = new Intent(
-                        android.content.Intent.ACTION_SEND);
+    public void onPrepareOptionsMenu(Menu menu) {
+        //gdamnit android
+        menu.clear();
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.about_menu, menu);
 
-                emailIntent.setType("plain/text");
-                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
-                        new String[] { "decad3nce@cyanogenmod.org" });
-                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-                        "aeGis Questions");
-                startActivity(Intent.createChooser(emailIntent, "Email"));
-                break;
-            case R.id.githubclick:
-                url = "http://www.github.com/Decad3nce/Aegis";
-                i = new Intent(Intent.ACTION_VIEW);
+        MenuItem item = menu.findItem(R.id.about_menu_share);
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        Intent i;
+        i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_SUBJECT, "aeGis");
+        String mInfo = "\nCheck out aeGis!\n\n";
+        mInfo = mInfo + "https://play.google.com/store/apps/details?id=com.decad3nce.aegis \n\n";
+        i.putExtra(Intent.EXTRA_TEXT, mInfo);
+
+        setShareIntent(i);
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    private String getResourceString(int id) {
+        String resourceString = getActivity().getResources().getString(id);
+        return resourceString;
+    }
+
+    private void setShareIntent(Intent shareIntent) {
+        if(mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(shareIntent);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.about_menu_quickly:
+                String url = "market://search?q=pub:Decad3nce";
+                Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
-                break;
-            case R.id.shareclick:
-                i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_SUBJECT, "aeGis");
-                String mInfo = "\nCheck out aeGis!\n\n";
-                mInfo = mInfo + "https://play.google.com/store/apps/details?id=com.decad3nce.aegis \n\n";
-                i.putExtra(Intent.EXTRA_TEXT, mInfo);
-                startActivity(Intent.createChooser(i, "Choose App"));
-                break;
-            case R.id.xdaclick:
-                url = "http://forum.xda-developers.com/showthread.php?t=2038762";
-                i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-                break;
-            case R.id.quicklyclick:
-                url = "https://play.google.com/store/apps/details?id=com.decad3nce.quickly";
-                i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse(url));
-                startActivity(i);
-                break;
+                return true;
+        }
+        return false;
+    }
+
+    private class aboutImageAdapter extends ArrayAdapter<String> {
+        private final Context context;
+        private final String[] values;
+        Typeface tf;
+
+
+        public aboutImageAdapter(Context context, String[] values) {
+            super(context, R.layout.about_list_item_layout, values);
+            this.context = context;
+            this.values = values;
+            this.tf = Typeface.createFromAsset(context.getAssets(), "Roboto-Light.ttf");
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowView = inflater.inflate(R.layout.about_list_item_layout, parent, false);
+            TextView textView = (TextView) rowView.findViewById(R.id.about_text_view);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.about_image_view);
+
+            textView.setText(values[position]);
+            textView.setTypeface(tf);
+
+            String s = values[position];
+            if(s.contains(getResourceString(R.string.about_email_link))) {
+                imageView.setImageResource(R.drawable.ic_content_email);
+            } else if(s.contains(getResourceString(R.string.about_github_link))) {
+                imageView.setImageResource(R.drawable.ic_github);
+            } else {
+                imageView.setImageResource(R.drawable.ic_xda);
+            }
+
+            return rowView;
         }
     }
 
